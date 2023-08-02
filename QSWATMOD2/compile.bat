ce65dd6cc1494dd98aa3396a72b011e5f896845b
Seonggyu Park <envpsg@gmail.com>
2023-08-02 16:21:50 -0500

    fixed issues #5 and #6

--- QSWATMOD2/compile.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-call "C:\OSGeo4W64\bin\o4w_env.bat"
-call "C:\OSGeo4W64\bin\qt5_env.bat"
-call "C:\OSGeo4W64\bin\py3_env.bat"
-
-@echo on
-pyrcc5 -o resources.py resources.qrc
-
--- QSWATMOD2/modules/flopy/version.py
+++
@@ -1,7 +0,0 @@
-# flopy version file automatically created using...make-release.py
-# created on...December 14, 2019 12:51:02
-
-major = 3
-minor = 3
-micro = 0
-__version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
